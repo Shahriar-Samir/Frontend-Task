5b97ef7a5c74970cd919e031c2c57cc668af7f07
Shahriar-Samir <shabusiness035@gmail.com>
2024-12-01 12:41:22 +0600

    add: navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="">
+      <h1>Logo</h1>
+      <ul>
+        <li>
+          <a href="#">Courses</a>
+        </li>
+        <li>
+          <a href="#">Instructions</a>
+        </li>
+        <li>
+          <a href="#">Blogs</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <section>
+        <img src="/icons/bag.svg" />
+        <button>Login</button>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
